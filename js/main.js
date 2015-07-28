edc7c78d854f7df1b611e612fbbe82390e4cbb72
Jether Gamboa <gamboajetherjosh@gmail.com>
2015-07-28 14:59:54 +0800

    modified main.js anf index.html

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ var clicks = 0;
 function clickME() {
     clicks += 1;
     
-    document.getElementById("clicks").innerHTML = clicks;
+    //document.getElementById("clicks").innerHTML = clicks;
     $(".monster").toggleClass('blue-monster');
     if (clicks >= 60){
         
         alert('\
             **************************************\n\n\
                 Congratulations\n\n\
-                You have defeated the Montster!\n\n\
+                You have defeated the Monster!\n\n\
             **************************************');
         window.location.replace('win.html');
         clicks = 0;
-    document.getElementById("clicks").innerHTML = clicks;
+    //document.getElementById("clicks").innerHTML = clicks;
         
     }
 }
